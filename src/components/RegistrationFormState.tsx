34f33f51d861ded42c7284bb83cd030dd74541cf
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-27 16:02:08 +0100

    feat: with generic handler

--- src/components/RegistrationFormState.tsx
+++ src/components/RegistrationFormState.tsx
@@ -5,33 +5,57 @@ import {
 } from "react";
 import { Button } from "../ui";
 
+type RegistrationFormDto = {
+  email: string;
+  password: string;
+  language: string;
+  country: string;
+};
+
 export const RegistrationFormState = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [language, setLanguage] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [language, setLanguage] = useState("");
+  const [state, setState] = useState<RegistrationFormDto>({
+    email: "",
+    password: "",
+    language: "",
+    country: "",
+  });
 
   const handleSubmit: FormEventHandler = (event) => {
     event.preventDefault();
   };
 
-  const handleEmailChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+  // const handleEmailChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+  //   const value = event.currentTarget.value;
+  //   setEmail(value);
+  // };
+
+  // const handlePasswordChange: ChangeEventHandler<HTMLInputElement> = (
+  //   event
+  // ) => {
+  //   const value = event.currentTarget.value;
+  //   setPassword(value);
+  // };
+
+  // const handleLanguageChange: ChangeEventHandler<HTMLInputElement> = (
+  //   event
+  // ) => {
+  //   const value = event.currentTarget.value;
+  //   setLanguage(value);
+  // };
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const name = event.currentTarget.name;
     const value = event.currentTarget.value;
-    setEmail(value);
+
+    // this.setState({ email: value })
+    const stateCopy = { ...state, [name]: value }; // ...rest
+    setState(stateCopy);
   };
 
-  const handlePasswordChange: ChangeEventHandler<HTMLInputElement> = (
-    event
-  ) => {
-    const value = event.currentTarget.value;
-    setPassword(value);
-  };
-
-  const handleLanguageChange: ChangeEventHandler<HTMLInputElement> = (
-    event
-  ) => {
-    const value = event.currentTarget.value;
-    setLanguage(value);
-  };
+  const { email, language, password } = state;
 
   return (
     <div>
@@ -43,12 +67,7 @@ export const RegistrationFormState = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">E-mail</label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            onChange={handleEmailChange}
-          />
+          <input id="email" name="email" type="email" onChange={handleChange} />
         </div>
         <div>
           <label htmlFor="password">Password</label>
@@ -56,16 +75,16 @@ export const RegistrationFormState = () => {
             id="password"
             name="password"
             type="password"
-            onChange={handlePasswordChange}
+            onChange={handleChange}
           />
         </div>
         <div>
           <label htmlFor="language">Language</label>
-          <input
-            id="language"
-            name="language"
-            onChange={handleLanguageChange}
-          />
+          <input id="language" name="language" onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="country">Country</label>
+          <input id="country" name="country" onChange={handleChange} />
         </div>
         <div>
           <Button type="submit">Submit</Button>
