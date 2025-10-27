6d8b43a98b10d1df7d72002e9be5e66dc2e39b0c
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-27 11:34:09 +0100

    feat: with barrels

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 // import { Fragment } from 'react';
 import "./App.css";
 // import Kaczka from "./ui/Text/Text";
-import { Text } from "./ui/Text/Text";
+// import { Text } from "./ui/Text/Text";
+// import { Text } from "./ui/Text";
+import { Text } from "./ui";
+// import { Text } from './ui' -> '@/ui' -> '@bos/ui`
 
 function App() {
   return (
     <>
       <div>
-        <Text>Test1</Text>
+        <Text>test</Text>
       </div>
     </>
   );
--- src/ui/Text/Text.tsx
+++ src/ui/Text/Text.tsx
@@ -1,10 +1,11 @@
 type Props = {
   children: string; // string / number / boolean
+  // children: React.ReactNode;
 };
 
-function Text(props: Props) {
+function Text({ children }: Props) {
   // [], undefined, null, 0, 1
-  const { children } = props; // { parsed_text }
+  // const { children } = props; // { parsed_text }
   // const parsed_text = props.parsed_text;
   return <p>{children}</p>;
 }
