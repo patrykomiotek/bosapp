797dae69a15b67fd0dd5709aaaa65e20c2a41b65
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-28 11:12:36 +0100

    feat: with forwardRef

--- src/ui/Input/Input.tsx
+++ src/ui/Input/Input.tsx
@@ -1,16 +1,25 @@
-import { type ComponentProps, useId } from "react";
+import {
+  type ComponentProps,
+  useId,
+  forwardRef,
+  type ForwardedRef,
+} from "react";
 
 type Props = {
   label?: string;
 } & ComponentProps<"input">;
 
-export const Input = ({ label, id, ...rest }: Props) => {
-  const reactId = useId();
-  const elemId = id ? id : reactId;
-  return (
-    <div>
-      <label htmlFor={elemId}>{label}</label>
-      <input id={elemId} {...rest} />
-    </div>
-  );
-};
+// Works in React 19
+// Doesn't work in React < 19
+export const Input = forwardRef(
+  ({ label, id, ...rest }: Props, ref: ForwardedRef<HTMLInputElement>) => {
+    const reactId = useId();
+    const elemId = id ? id : reactId;
+    return (
+      <div>
+        <label htmlFor={elemId}>{label}</label>
+        <input ref={ref} id={elemId} {...rest} />
+      </div>
+    );
+  }
+);
