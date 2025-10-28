1850a73a97ce99d38ad4fa87190eebcf7eb0d476
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-28 12:31:07 +0100

    feat: with magic button basic version

--- src/ui/MagicButton/MagicButton.tsx
+++ src/ui/MagicButton/MagicButton.tsx
@@ -1,10 +1,11 @@
-import { type ComponentProps, useEffect } from "react";
+import { type ComponentProps, useEffect, useRef } from "react";
 
 const palette = {
   emerald: "#2ecc71",
   midnightBlue: "#2c3e50",
   clouds: "#ecf0f1",
   carrot: "#e67e22",
+  peterRiver: "#3498db",
 };
 
 type Color = keyof typeof palette;
@@ -22,22 +23,40 @@ const MagicButton = ({
 }: Props) => {
   // const [state, setState] = useState();
   // const refFiled = useRef();
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     // logic here
     console.log("mounting");
+    if (buttonRef.current) {
+      buttonRef.current.style.backgroundColor = "#0000ff";
+    }
 
     return () => {
       console.log("unmounting");
     };
   }, []);
 
+  const handleMouseEnter = () => {
+    if (buttonRef.current) {
+      buttonRef.current.style.backgroundColor = "#ff0000";
+    }
+  };
+  const handleMouseLeave = () => {
+    if (buttonRef.current) {
+      buttonRef.current.style.backgroundColor = "#00ff00";
+    }
+  };
+
   return (
     <button
+      ref={buttonRef}
       style={{
         color: palette[color],
         backgroundColor: palette[bgColor],
       }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       {...rest}
     >
       {children}
