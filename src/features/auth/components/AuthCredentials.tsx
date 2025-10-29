673ee9b2066b508c35a7101ded04e0a3f17e278a
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-29 11:15:36 +0100

    feat: with callback

--- src/features/auth/components/AuthCredentials.tsx
+++ src/features/auth/components/AuthCredentials.tsx
@@ -4,10 +4,17 @@ import { useAuthContext } from "./AuthContext";
 export const AuthCredentials = () => {
   const context = useAuthContext();
 
+  // throw new Error("new error");
+
+  const handleClick = () => {
+    // throw new Error("callback error");
+    context.toggle();
+  };
+
   return (
     <div>
       <p>Is user logged in? {context.isLoggedIn ? "YES" : "NO"}</p>
-      <Button onClick={() => context.toggle()}>Toggle</Button>
+      <Button onClick={handleClick}>Toggle</Button>
     </div>
   );
 };
